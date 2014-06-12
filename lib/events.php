952ff6cbde953c809ac0f09356c7c15f8806596a
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-06-12 23:51:09 +0200

    Fix language strings

--- lib/events.php
+++ lib/events.php
@@ -8,9 +8,9 @@ function stripe_connect_pagesetup() {
 	$user = elgg_get_page_owner_entity();
 
 	elgg_register_menu_item('page', array(
-		'name' => 'stripe:connect:profile',
+		'name' => 'stripe:connect:user:profile',
 		'href' => "stripe_connect/connect/$user->username",
-		'text' => elgg_echo('stripe:connect:profile'),
+		'text' => elgg_echo('stripe:connect:user:profile'),
 		'selected' => (substr_count(current_page_url(), 'stripe_connect/profile')),
 		'context' => 'settings',
 		'section' => 'stripe',
